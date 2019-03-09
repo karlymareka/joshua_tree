b40fde013e56d1c36f7574ff4d5f7393e9977c22
Karly Mareka <karly.mareka@gmail.com>
2019-03-09 02:53:59 +0000

    started CLI class

--- lib/joshua_tree.rb
+++ lib/joshua_tree.rb
@@ -2,5 +2,15 @@ require "joshua_tree/version"
 
 module JoshuaTree
   class Error < StandardError; end
-  # Your code goes here...
+  require 'pry'
+  require 'nokogiri'
+
+class CLI 
+  puts "Welcome! To learn more information about camping options at Joshua Tree National Park, enter 1 for Reservation Only campgrounds or enter 2 for Walk-In Only campgrounds."
+  campground_type = gets.strip
+  #program displays campgrounds of that type
+
+  puts "Enter the number of the campground to learn more." 
+
+end .
 end
